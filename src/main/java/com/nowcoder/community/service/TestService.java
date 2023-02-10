56c29ad495ef34d6b12d498b5690d2cfe028a8df
zhangyu <1802833394@qq.com>
2023-02-10 15:36:19 +0800

    手动管理与获取第三方Bean-偏底层

---
+++ src/main/java/com/nowcoder/community/service/TestService.java
@@ -0,0 +1,27 @@
+package com.nowcoder.community.service;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+@Service // 业务注解
+//@Scope("prototype") 多例
+public class TestService {
+
+    public TestService() {
+        System.out.println("实例化TestService");
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("初始化TestService");
+    }
+
+    @PreDestroy
+    public void destory() {
+        System.out.println("销毁TestService");
+    }
+
+}
--- src/test/java/com/nowcoder/community/CommunityApplicationTests.java
+++ src/test/java/com/nowcoder/community/CommunityApplicationTests.java
@@ -1,14 +1,19 @@
 package com.nowcoder.community;
 
 import com.nowcoder.community.dao.testDao;
+import com.nowcoder.community.service.TestService;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.BeansException;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.ApplicationListener;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
@@ -33,5 +38,21 @@ class CommunityApplicationTests implements ApplicationContextAware {
         // 面向接口编程
         testDao dao = applicationContext.getBean(testDao.class);
         System.out.println(dao.select());
+    }
+
+    @Test
+    public void testService() {
+        TestService service = applicationContext.getBean(TestService.class);
+        System.out.println(service);
+//        service.init();
+//        service.destory();
+    }
+
+    @Test
+    public void testConfig() {
+        SimpleDateFormat simpleDateFormat =
+                applicationContext.getBean(SimpleDateFormat.class);
+
+        System.out.println(simpleDateFormat.format(new Date()));
     }
 }
