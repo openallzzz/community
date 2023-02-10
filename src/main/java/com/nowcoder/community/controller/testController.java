e07ad0f59c774a236dce89585c2525f749bf5006
zhangyu <1802833394@qq.com>
2023-02-10 14:57:50 +0800

    基础工程

--- src/main/java/com/nowcoder/community/controller/testController.java
+++ src/main/java/com/nowcoder/community/controller/testController.java
@@ -1,2 +1,17 @@
-package com.nowcoder.community.controller;public class testController {
+package com.nowcoder.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/test")
+public class testController {
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "Hello World！";
+    }
+
 }
