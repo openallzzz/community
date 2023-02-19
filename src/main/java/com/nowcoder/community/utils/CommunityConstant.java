bcb1214c169e424eb89d3d49c1cf13d7315a5626
zhangyu <1802833394@qq.com>
2023-02-19 14:22:00 +0800

    开发网站注册功能（发送激活邮件、邮件激活）

---
+++ src/main/java/com/nowcoder/community/utils/CommunityConstant.java
@@ -0,0 +1,11 @@
+package com.nowcoder.community.utils;
+
+public interface CommunityConstant {
+
+    int ACTIVATION_SUCCESS = 0;
+
+    int ACTIVATION_REPEAT = 1;
+
+    int ACTIVATION_FAILURE = 2;
+
+}
