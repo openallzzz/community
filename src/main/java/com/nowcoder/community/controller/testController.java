8feb60f564be3114f2d297d6191797d0ce17d48f
zhangyu <1802833394@qq.com>
2023-02-24 17:11:26 +0800

    获取cookie

--- src/main/java/com/nowcoder/community/controller/testController.java
+++ src/main/java/com/nowcoder/community/controller/testController.java
@@ -2,6 +2,7 @@ package com.nowcoder.community.controller;
 
 import com.nowcoder.community.utils.CommunityUtil;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -28,6 +29,13 @@ public class testController {
         cookie.setMaxAge(10 * 60);
         response.addCookie(cookie);
         return "set cookie";
+    }
+
+    @RequestMapping(path = "/cookie/get", method = RequestMethod.GET)
+    @ResponseBody
+    public String getCookie(@CookieValue("code") String code) {
+        System.out.println(code);
+        return "get cookie";
     }
 
 }
