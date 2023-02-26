407805b11025368605706e8084c5788f10136b41
zhangyu <1802833394@qq.com>
2023-02-26 22:51:12 +0800

    使用拦截器完成网站头部登录信息的正确显示

---
+++ src/main/java/com/nowcoder/community/controller/interceptor/AlphaInterceptor.java
@@ -0,0 +1,11 @@
+package com.nowcoder.community.controller.interceptor;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+public class AlphaInterceptor implements HandlerInterceptor {
+
+
+
+}
---
+++ src/main/java/com/nowcoder/community/utils/CookieUtil.java
@@ -0,0 +1,25 @@
+package com.nowcoder.community.utils;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+public class CookieUtil {
+
+    public static String getValue(HttpServletRequest request, String key) {
+        if(request == null || key == null) {
+            throw new IllegalArgumentException("参数为空！");
+        }
+
+        Cookie[] cookies = request.getCookies();
+        if(cookies != null) {
+            for (Cookie cookie : cookies) {
+                if(cookie.getName().equals(key)) {
+                    return cookie.getValue();
+                }
+            }
+        }
+
+        return null;
+    }
+
+}
