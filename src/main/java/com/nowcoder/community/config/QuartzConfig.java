2af484b09516f2f63421573a157c2b8c5d8cff0b
zhangyu <1802833394@qq.com>
2023-04-17 21:42:46 +0800

    JDK、Spring、Quartz线程池和可执行定时任务的线程池的使用

---
+++ src/main/java/com/nowcoder/community/config/QuartzConfig.java
@@ -0,0 +1,38 @@
+package com.nowcoder.community.config;
+
+import com.nowcoder.community.quartz.AlphaJob;
+import org.quartz.JobDataMap;
+import org.quartz.JobDetail;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.quartz.JobDetailFactoryBean;
+import org.springframework.scheduling.quartz.SimpleTriggerFactoryBean;
+
+import javax.xml.stream.FactoryConfigurationError;
+
+@Configuration
+public class QuartzConfig {
+
+//    @Bean
+    public JobDetailFactoryBean alphaJobDetail() {
+        JobDetailFactoryBean factoryBean = new JobDetailFactoryBean();
+        factoryBean.setJobClass(AlphaJob.class);
+        factoryBean.setName("alphaJob");
+        factoryBean.setGroup("alphaJobGroup");
+        factoryBean.setDurability(true);
+        factoryBean.setRequestsRecovery(true);
+        return factoryBean;
+    }
+
+//    @Bean
+    public SimpleTriggerFactoryBean alphaTrigger(JobDetail alphaJobDetail) {
+        SimpleTriggerFactoryBean factoryBean = new SimpleTriggerFactoryBean();
+        factoryBean.setJobDetail(alphaJobDetail);
+        factoryBean.setName("alphaTrigger");
+        factoryBean.setGroup("alphaTriggerGroup");
+        factoryBean.setRepeatInterval(3000);
+        factoryBean.setJobDataMap(new JobDataMap());
+        return factoryBean;
+    }
+
+}
--- src/main/java/com/nowcoder/community/service/TestService.java
+++ src/main/java/com/nowcoder/community/service/TestService.java
@@ -1,12 +1,14 @@
 package com.nowcoder.community.service;
 
 import org.springframework.context.annotation.Scope;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
-//@Service // 业务注解
+@Service // 业务注解
 //@Scope("prototype") 多例
 public class TestService {
 
@@ -22,6 +24,16 @@ public class TestService {
     @PreDestroy
     public void destory() {
         System.out.println("销毁TestService");
+    }
+
+    @Async
+    public void execute1() {
+        System.out.println("execute1");
+    }
+
+    @Scheduled(initialDelay = 10000, fixedRate = 1000)
+    public void execute2() {
+        System.out.println("execute2");
     }
 
 }
