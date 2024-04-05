653492db8ca8e495b2c999589e1e42165b932b19
zhangyu <zhangyu@songshuai.com>
2024-04-05 18:53:22 +0800

    fix:底部信息更改

--- src/main/java/com/nowcoder/community/aspect/ServiceLogAspect.java
+++ src/main/java/com/nowcoder/community/aspect/ServiceLogAspect.java
@@ -1,11 +1,13 @@
 package com.nowcoder.community.aspect;
 
+import com.nowcoder.community.utils.HostHolder;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
@@ -19,6 +21,9 @@ import java.util.Date;
 public class ServiceLogAspect {
 
     private static final Logger logger = LoggerFactory.getLogger(ServiceLogAspect.class);
+
+    @Autowired
+    private HostHolder hostHolder;
 
     @Pointcut("execution(* com.nowcoder.community.service.*.*(..))")
     public void pointcut() {
@@ -36,11 +41,14 @@ public class ServiceLogAspect {
         HttpServletRequest request = attributes.getRequest();
 
         String host = request.getRemoteHost();
+        String username = null;
+        if(hostHolder.getUser() != null) {
+            username = hostHolder.getUser().getUsername();
+        }
         String now = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
-
         String target = joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName();
 
-        logger.info(String.format("用户[%s],在[%s],访问了[%s].", host, now, target));
+        logger.info(String.format("用户[%s], ip[%s],在[%s],访问了[%s].", username, host, now, target));
     }
 
 }
